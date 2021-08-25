0d5e3de5a1b295e30ac90ac7fa70fb0b0c5ea6a4
mine-cpp <paul0eiry0@gmail.com>
2021-08-24 21:19:22 -0300

    Updated Event Handler

--- include/fard/event.hpp
+++ include/fard/event.hpp
@@ -9,7 +9,7 @@ class fard::Event_handler {
 public:
   struct Mouse {
     int x, y;
-    uint32 buttons;
+    uint32 on_buttons;
   };
 
 private:
@@ -19,26 +19,28 @@ private:
   bool running__;
   bool paused__;
   event_t event__;
-  key_array_t keys__;
+  key_array_t on_keys__;
   Mouse mouse__;
 
 public:
   bool is_running() const { return running__; }
   bool is_paused() const { return paused__; }
-  const uint8* keys() const { return keys__; }
-  Mouse mouse() const { return mouse__; }
 
-  bool key_pressed(const keycode_t keycode) { return keys__[keycode]; }
-  bool key_not_pressed(const keycode_t keycode) { return !(keys__[keycode]); }
+  bool key_on(const keycode_t keycode) { return on_keys__[keycode]; }
+  bool key_off(const keycode_t keycode) { return !(on_keys__[keycode]); }
 
-  static void handle() {
+  int mouse_x() const { return mouse.x; }
+  int mouse_y() const { return mouse.y; }
+  bool buttons_on(const uint32 buttons) const { return (mouse__.on_buttons & buttons); }
+
+  void handle() {
 
     while (SDL_PollEvent(&event__)) {
 
     }
 
-    keys__ = const_cast<uint8*>(SDL_GetKeyboardState(nullptr));
-    mouse__.buttons = SDL_GetMouseState(&(mouse__.x), &(mouse__.y));
+    on_keys__ = const_cast<key_array_t>(SDL_GetKeyboardState(nullptr));
+    mouse__.on_buttons = SDL_GetMouseState(&(mouse__.x), &(mouse__.y));
 
   }
 
