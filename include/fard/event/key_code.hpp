17ea0fee55aa1db63400708d37fb178fc0acaf3c
mine-cpp <paul0eiry0@gmail.com>
2021-08-31 09:34:14 -0300

    Added more keys to key_code enum

--- include/fard/event/key_code.hpp
+++ include/fard/event/key_code.hpp
@@ -41,7 +41,59 @@ enum class fard::key_code {
     key_7 = SDL_SCANCODE_7,
     key_8 = SDL_SCANCODE_8,
     key_9 = SDL_SCANCODE_9,
-    key_0 = SDL_SCANCODE_0
+    key_0 = SDL_SCANCODE_0,
+
+    key_return = SDL_SCANCODE_RETURN,
+    escape = SDL_SCANCODE_ESCAPE,
+    backspace = SDL_SCANCODE_BACKSPACE,
+    tab = SDL_SCANCODE_TAB,
+    space = SDL_SCANCODE_SPACE,
+
+    minus = SDL_SCANCODE_MINUS,
+    equals = SDL_SCANCODE_EQUALS,
+    left_bracket = SDL_SCANCODE_LEFTBRACKET,
+    right_bracket = SDL_SCANCODE_RIGHTBRACKET,
+    backslash = SDL_SCANCODE_BACKSLASH,
+    nonushash = SDL_SCANCODE_NONUSHASH,
+
+    semicolon = SDL_SCANCODE_SEMICOLON,
+    apostrophe = SDL_SCANCODE_APOSTROPHE,
+    grave = SDL_SCANCODE_GRAVE,
+
+    comma = SDL_SCANCODE_COMMA,
+    period = SDL_SCANCODE_PERIOD,
+    slash = SDL_SCANCODE_SLASH,
+
+    capslock = SDL_SCANCODE_CAPSLOCK,
+
+    f1 = SDL_SCANCODE_F1,
+    f2 = SDL_SCANCODE_F2,
+    f3 = SDL_SCANCODE_F3,
+    f4 = SDL_SCANCODE_F4,
+    f5 = SDL_SCANCODE_F5,
+    f6 = SDL_SCANCODE_F6,
+    f7 = SDL_SCANCODE_F7,
+    f8 = SDL_SCANCODE_F8,
+    f9 = SDL_SCANCODE_F9,
+    f10 = SDL_SCANCODE_F10,
+    f11 = SDL_SCANCODE_F11,
+    f12 = SDL_SCANCODE_F12,
+
+    printscreen = SDL_SCANCODE_PRINTSCREEN,
+    scrolllock = SDL_SCANCODE_SCROLLLOCK,
+    pause = SDL_SCANCODE_PAUSE,
+    insert = SDL_SCANCODE_INSERT,
+
+    home = SDL_SCANCODE_HOME,
+    pageup = SDL_SCANCODE_PAGEUP,
+    pagedown = SDL_SCANCODE_PAGEDOWN,
+    key_delete = SDL_SCANCODE_DELETE,
+    end = SDL_SCANCODE_END,
+
+    right = SDL_SCANCODE_RIGHT,
+    left = SDL_SCANCODE_LEFT,
+    down = SDL_SCANCODE_DOWN,
+    up = SDL_SCANCODE_UP
 
   // TODO
   // add more keys
