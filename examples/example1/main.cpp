c18feaa19f83eab2551a58e66eb781722f714140
mine-cpp <paul0eiry0@gmail.com>
2021-08-31 08:12:39 -0300

    Updated example

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -2,16 +2,20 @@
 #include <SDL2/SDL_image.h>
 #include <fard/window.hpp>
 #include <fard/event.hpp>
+#include <fard/image.hpp>
 
 int main(int argc, char* argv[]) {
   SDL_Init(SDL_INIT_EVERYTHING);
   IMG_Init(IMG_INIT_PNG);
 
-  fard::window::properties properties = { "fard window" };
+  fard::window::properties properties = { };
   fard::window window = fard::window(properties);
 
   fard::event_checker checker = fard::event_checker();
   fard::event_handler handler = fard::event_handler(checker);
+
+  fard::image vr = { };
+  vr.texture = window.load_texture("virtual_riot.png");
 
   bool running = true;
 
@@ -22,6 +26,7 @@ int main(int argc, char* argv[]) {
       running = false;
 
     window.clear();
+    window.draw(vr);
     window.update();
 
   }
