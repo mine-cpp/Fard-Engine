7e2ef8f8c9da699f9d4ec4abe1b655a388bcdf55
mine-cpp <paul0eiry0@gmail.com>
2021-09-01 13:06:33 -0300

    Added initializing for SDL_mixer

--- src/core.cpp
+++ src/core.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #include <fard/core.hpp>
 
 static constexpr int sdl_flags = SDL_INIT_EVERYTHING;
 static constexpr int img_flags = IMG_INIT_PNG |
   IMG_INIT_PNG |
   IMG_INIT_TIF;
+static constexpr int mix_flags = MIX_INIT_FLAC |
+  MIX_INIT_MOD |
+  MIX_INIT_MP3 |
+  MIX_INIT_OGG;
 
 static void init_sdl() {
   bool initted = !(SDL_Init(sdl_flags));
@@ -18,11 +23,17 @@ static void init_img() {
   if (!initted)
     throw "ERROR: Could not initialize SDL_image";
 }
+static void init_mix() {
+  bool initted = (Mix_Init(mix_flags) & mix_flags);
+  if (!initted)
+    throw "ERROR: Could not initialize SDL_mixer";
+}
 
 void fard::init() {
   try {
     init_sdl();
     init_img();
+    init_mix();
   }
   catch(const char* error) {
     std::cerr << error << std::endl;
@@ -32,4 +43,8 @@ void fard::init() {
 void fard::quit() {
   SDL_Quit();
   IMG_Quit();
+
+  while(Mix_Init(0))
+    Mix_Quit();
+
 }
