cc5050baa3db386c10d73a6680acff8529e7abaa
mine-cpp <paul0eiry0@gmail.com>
2021-09-02 17:10:43 -0300

    Updated example

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <SDL2/SDL_image.h>
+#include <fard/core.hpp>
 #include <fard/window.hpp>
 #include <fard/event.hpp>
 #include <fard/image.hpp>
 
 int main(int argc, char* argv[]) {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  IMG_Init(IMG_INIT_PNG);
+  fard::init();
 
   fard::window::properties properties = { };
   fard::window window = fard::window(properties);
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
   while (running) {
 
     handler.handle();
-    if (checker.on(fard::event_type::window_closed))
+    if (checker.on(fard::event_type::window_close))
       running = false;
 
     window.clear();
@@ -31,7 +31,6 @@ int main(int argc, char* argv[]) {
 
   }
 
-  SDL_Quit();
-  IMG_Quit();
+  fard::quit();
   return 0;
 }
