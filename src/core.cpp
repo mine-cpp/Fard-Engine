46fadffeeeffd5b8dc19f52a3f3f60b79064017c
mine-cpp <paul0eiry0@gmail.com>
2021-09-02 16:27:33 -0300

    Updated core functions

--- src/core.cpp
+++ src/core.cpp
@@ -3,15 +3,21 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 #include <fard/core.hpp>
+#include <fard/utils/typedefs.hpp>
 
-static constexpr int sdl_flags = SDL_INIT_EVERYTHING;
-static constexpr int img_flags = IMG_INIT_PNG |
+static constexpr int sdl_flags = SDL_INIT_EVERYTHING,
+  img_flags = IMG_INIT_PNG |
   IMG_INIT_PNG |
-  IMG_INIT_TIF;
-static constexpr int mix_flags = MIX_INIT_FLAC |
+  IMG_INIT_TIF,
+  mix_flags = MIX_INIT_FLAC |
   MIX_INIT_MOD |
   MIX_INIT_MP3 |
   MIX_INIT_OGG;
+
+static constexpr int freq = 41100;
+static constexpr fard::uint16_t format = AUDIO_U16;
+static constexpr int channels = 2,
+  chunksize = 4096;
 
 static void init_sdl() {
   bool initted = !(SDL_Init(sdl_flags));
@@ -28,23 +34,26 @@ static void init_mix() {
   if (!initted)
     throw "ERROR: Could not initialize SDL_mixer";
 }
+static void init_audio() {
+  bool initted = !(Mix_OpenAudio(freq, format, channels, chunksize));
+  if (!initted)
+    throw "ERROR: Coul not initialize audio";
+}
 
 void fard::init() {
-  try {
-    init_sdl();
-    init_img();
-    init_mix();
-  }
-  catch(const char* error) {
-    std::cerr << error << std::endl;
-  }
-
+  init_sdl();
+  init_img();
+  init_mix();
+  init_audio();
 }
 void fard::quit() {
-  SDL_Quit();
-  IMG_Quit();
+  int times_opened = Mix_QuerySpec(nullptr, nullptr, nullptr);
 
+  while(times_opened--)
+    Mix_CloseAudio();
   while(Mix_Init(0))
     Mix_Quit();
 
+  IMG_Quit();
+  SDL_Quit();
 }
